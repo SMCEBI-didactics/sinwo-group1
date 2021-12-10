36b46afd140cff546b6f5b93b2e951705eabd2ec
aszadzinski <albert.szadzinski@protonmail.com>
2021-12-10 06:07:11 +0100

    INIT flask app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WebApp
 ŚiNWO Aplikacja bazowa
+
+usage:
+
+```console
+user@host:~$ export FLASK_APP=app.py
+user@host:~$ export FLASK_ENV=development
+user@host:~$ flask run -h 0.0.0.0 -p 9999
+```
---
+++ WebApp/views.py
@@ -0,0 +1,20 @@
+from WebApp import app
+
+from flask import render_template, request, redirect, url_for
+
+@app.route("/")
+def home_route():
+    return render_template("index.html")
+
+@app.route("/api/<var>")
+def api_route(var):
+    return render_template("api.html", var=var)
+
+@app.route("/login", methods=["GET", "POST"])
+def login_route():
+    state = "Unauthenticated"
+    if request.method == "POST":
+        login = request.form["login"]
+        password = request.form["password"]
+        state = f"{state} {login}:{password}"
+    return render_template("login.html", state=state)
