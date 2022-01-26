d9dbd849a51c87e18cf745b876e34fc76ca2a478
Sassanek <adriano2250@gmail.com>
2022-01-26 20:54:48 +0100

    Dodanie funkcjonalnosci generujacej identity

--- WebApp/views.py
+++ WebApp/views.py
@@ -3,6 +3,7 @@ from WebApp.models import *
 from flask import render_template, request
 
 from Dodaj.main import dodaj
+from Identity.main import gen_identity
 
 """
 """
@@ -102,6 +103,7 @@ def login_route():
         state = f"{state} {login}:{password}"
     return render_template("login.html", state=state)
 
+
 @app.route("/prompt", methods=["GET", "POST"])
 def prompt():
     """
@@ -113,6 +115,21 @@ def prompt():
         prompt = eval(prompt)
         state = f"{prompt}"
     return render_template("prompt.html", state=state)
+
+@app.route("/identity", methods=["GET","POST"])
+def identity():
+    """
+    testuj polecenia 
+    """
+    state = "empty"
+    if request.method == "GET":
+        state = f"{request.remote_addr}"
+        return render_template("identity.html", state=state)
+    if request.method == "POST":
+        pesel,data_ur,miejce_zamieszkania,nr_telefonu,plec,kolor_wlosow,kolor_oczu = gen_identity()
+        return render_template("identity.html", pesel = pesel, data_ur = data_ur,miejce_zamieszkania = miejce_zamieszkania, nr_telefonu = nr_telefonu, plec = plec, kolor_wlosow = kolor_wlosow, kolor_oczu = kolor_oczu)
+    # return render_template("identity.html", state=state)
+
 
 
 
